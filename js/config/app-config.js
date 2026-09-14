e41605dff4b669ee8976bb3d7abe8d5db8359446
hasriandi basir <hasriandibasir80@guru.sd.belajar.id>
2026-09-14 10:36:40 +0800

    Add app-config.js with application configuration

---
+++ js/config/app-config.js
@@ -0,0 +1,25 @@
+export const AppConfig = {
+  SCHOOL_NAME: 'SDN 139 LAMANDA',
+  SCHOOL_YEAR: '2024/2025',
+  VERSION: '1.0.0',
+  
+  // Roles
+  ROLES: {
+    SUPER_ADMIN: 'super_admin',
+    KEPALA_SEKOLAH: 'kepala_sekolah',
+    STAF_TU: 'staf_tu',
+    WALI_KELAS: 'wali_kelas',
+    GURU_MAPEL: 'guru_mapel',
+    SISWA: 'siswa',
+    ORANG_TUA: 'orang_tua'
+  },
+  
+  // Features
+  FEATURES: {
+    E_DOKUMEN: 'e-dokumen',
+    ADMINISTRASI_GURU: 'administrasi-guru',
+    DATA_STATISTIK: 'data-statistik',
+    E_PORTAL: 'e-portal',
+    DATA_MASTER: 'data-master'
+  }
+};
