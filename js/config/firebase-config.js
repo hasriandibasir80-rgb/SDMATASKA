384093eeaca8b9cd5e8651162658cf56749a98ea
hasriandi basir <hasriandibasir80@guru.sd.belajar.id>
2026-09-14 15:38:21 +0800

    Create firebase-config.js

---
+++ js/config/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIMGboQ0_HUyE8gJiNH6qdXIFo_n_QBVI",
+  authDomain: "salamdmataska.firebaseapp.com",
+  projectId: "salamdmataska",
+  storageBucket: "salamdmataska.firebasestorage.app",
+  messagingSenderId: "60672688704",
+  appId: "1:60672688704:web:b600e865c19445ec7495a5",
+  measurementId: "G-ZHE0TR058H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
