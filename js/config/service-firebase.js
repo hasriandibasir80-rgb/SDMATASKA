211b114cbfef11c2ae217017512d8addee972aa6
hasriandi basir <hasriandibasir80@guru.sd.belajar.id>
2026-09-14 15:44:23 +0800

    Add Firebase service configuration file

---
+++ js/config/service-firebase.js
@@ -0,0 +1,8 @@
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js';
+import app from './firebase-config.js';
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
